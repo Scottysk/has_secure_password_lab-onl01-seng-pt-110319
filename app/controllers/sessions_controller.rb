e84a47fadb8052985b2ba44d9d6933feab88cfd7
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-05-01 02:33:05 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,10 @@ class SessionsController < ApplicationController
   def create 
     @user = User.find_by(name: params[:user][:name])
     @authenticated = @user.try(:authenticate, params[:user][:password])
-    if authenticated && @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
+    if authenticated
+      sessions[:user_id] = @user.id
       redirect_to :welcome
     else
-      flash[:notice] = "Login is incorrect"
       redirect_to :login
     end
   end
