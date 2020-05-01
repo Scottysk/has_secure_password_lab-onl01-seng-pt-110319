82b7d88d65d17edd479df4386c6818855b2e3afa
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-05-01 02:41:11 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,10 @@ class UsersController < ApplicationController
   
   def create
     @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to :welcome
+    else
+      redirect_to :new
   end
 end
