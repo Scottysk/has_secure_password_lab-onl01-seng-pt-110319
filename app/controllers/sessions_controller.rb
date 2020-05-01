72a21e0f4c4c3d91e5a8bf171a15beaf40454b6d
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-05-01 02:31:06 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,14 @@ class SessionsController < ApplicationController
   end
   
   def create 
-    @user = User.find_by()
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to :welcome
+    else
+      flash[:notice] = "Login is incorrect"
+      redirect_to :login
+    end
   end
   
   
