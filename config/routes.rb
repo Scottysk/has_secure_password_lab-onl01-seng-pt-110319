49525f6c1cda614b3bb651bcf5f902537a0c2acf
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-05-01 02:36:34 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get '/login' => 'sessions#new'
-  get 'login' => 'sessions#create'
+  post 'login' => 'sessions#create'
   get 'welcome' => 'welcome#new'
   
 end
