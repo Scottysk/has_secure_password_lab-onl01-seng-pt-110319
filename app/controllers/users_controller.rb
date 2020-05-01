6041be5cfc21b4e2836482406723318ee147ad42
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-05-01 02:42:16 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,7 @@ class UsersController < ApplicationController
       redirect_to :welcome
   end
   
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  
 end
