4acb70776fe6cc41dafa9c2b56ba18d210bcbc34
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-05-01 02:38:29 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   
   def create 
     @user = User.find_by(name: params[:user][:name])
-    @authenticated = @user.try(:authenticate, params[:user][:password])
-    if @authenticated
+    authenticated = @user.try(:authenticate, params[:user][:password])
+    if authenticated
       session[:user_id] = @user.id
       redirect_to :welcome
     else
